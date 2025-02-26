885729e14d1c956b0bab771327a3532e6c5dae9a
Minjo Park <consistent.312@gmail.com>
2025-02-27 08:50:01 +0900

    add: Router 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Router from "./shared/Router";
+
 function App() {
-  return <div>Hello</div>;
+  return <Router/>;
 }
 
 export default App;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import { PATH } from '../constants/RouterPathConstants';
 import Login from '../pages/Login';
@@ -9,19 +9,21 @@ import MyPage from '../pages/MyPage';
 import ResumeListPage from '../pages/ResumeListPage';
 import ResumeCreate from '../pages/ResumeCreate';
 
-const AppRouter = () => {
+const Router = () => {
   return (
+    <BrowserRouter>
       <Routes>
         <Route path={PATH.HOME} element={<Home />} />
-        <Route path={PATH.LOGIN} element={<Login/>}/>
-        <Route path={PATH.SIGNUP} element={<Signup/>}/>
-        <Route path={PATH.JOB_LIST} element={<JobListPage/>}/>
-        <Route path={PATH.JOB_DETAIL} element={<JobDetail/>}/>
-        <Route path={PATH.MY_PAGE} element={<MyPage/>}/>
-        <Route path={PATH.RESUME_LIST} element={<ResumeListPage/>}/>
-        <Route path={PATH.RESUME_CREATE} element={<ResumeCreate/>}/>
+        <Route path={PATH.LOGIN} element={<Login />} />
+        <Route path={PATH.SIGNUP} element={<Signup />} />
+        <Route path={PATH.JOB_LIST} element={<JobListPage />} />
+        <Route path={PATH.JOB_DETAIL} element={<JobDetail />} />
+        <Route path={PATH.MY_PAGE} element={<MyPage />} />
+        <Route path={PATH.RESUME_LIST} element={<ResumeListPage />} />
+        <Route path={PATH.RESUME_CREATE} element={<ResumeCreate />} />
       </Routes>
+    </BrowserRouter>
   );
 };
 
-export default AppRouter;
+export default Router;
