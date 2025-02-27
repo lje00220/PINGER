6a6e9bd9b258920aa9e7f96aca721cdf6f09dbc7
Minjo Park <consistent.312@gmail.com>
2025-02-27 20:24:03 +0900

    fix: constant로 변환

--- src/utils/SliceTitleLength.js
+++ src/utils/SliceTitleLength.js
@@ -1,7 +1,9 @@
+const START_INDEX = 0;
 const SLICE_INDEX = 20;
+const DOT = '...';
 
 const sliceTitleLength = (title) => {
-  const sliceTitle = title.slice(0, SLICE_INDEX) + '...';
+  const sliceTitle = title.slice(START_INDEX, SLICE_INDEX) + DOT;
   return sliceTitle;
 };
 
