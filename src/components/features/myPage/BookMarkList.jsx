848cf38f73340328c06516284e04a6d97a4a424f
Minjo Park <consistent.312@gmail.com>
2025-02-27 15:47:12 +0900

    add: 채용정보 공통 컴포넌트 파일 추가

--- src/components/features/myPage/BookMarkList.jsx
+++ src/components/features/myPage/BookMarkList.jsx
@@ -1,7 +1,10 @@
+import JobItem from '../../common/jobItem';
+
 const BookMarkList = () => {
   return (
     <div className="flex flex-col items-center gap-4">
       <h1 className="text-2xl">북마크</h1>
+      <JobItem />
     </div>
   );
 };
