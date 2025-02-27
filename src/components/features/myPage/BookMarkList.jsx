3e82116768424271cdc5451f690c9c16048f7ca5
Minjo Park <consistent.312@gmail.com>
2025-02-27 17:18:59 +0900

    chore: 더미 데이터 추가

--- src/components/features/myPage/BookMarkList.jsx
+++ src/components/features/myPage/BookMarkList.jsx
@@ -9,11 +9,17 @@ const BookMarkList = () => {
       start_date: '20250205',
       end_date: '20250212',
     },
+    {
+      id: 2,
+      company_name: '서울대학교병원',
+      start_date: '20250205',
+      end_date: '20250212',
+    },
   ];
 
   /** UI */
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col items-center gap-5">
       <h1 className="text-2xl">북마크</h1>
       {jobs.map((job) => {
         return <JobItem key={job.id} job={job} width={600} />;
