55d0d521e52fb88cdab969177ff29d8f1fdb8f9d
woozizi <c980805@naver.com>
2025-02-27 22:05:41 +0900

    styles: 자소서  css수정

--- src/pages/ResumeDetail.jsx
+++ src/pages/ResumeDetail.jsx
@@ -1,10 +1,19 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { dummyData, useUserStore } from './ResumeListPage';
+import JobInfo from '../components/common/JobInfo';
+import { useJobsQuery } from '../hooks/useJobsQuerys';
 
 const ResumeDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const { role } = useUserStore();
+  const { data: jobData, isPending, isError } = useJobsQuery();
+
+  if (isPending) return <div className="p-4 text-center">로딩 중...</div>;
+  if (isError)
+    return <div className="p-4 text-center">데이터 불러오기 실패</div>;
+
+  const targetJob = jobData.find((job) => job.id === parseInt(id));
 
   const handleEdit = () => {
     // 수정 페이지로 이동
@@ -24,47 +33,86 @@ const ResumeDetail = () => {
   const resume = dummyData.find((item) => item.id === Number(id));
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center bg-my-bg">
-      <div className="mt-10 h-64 w-full max-w-lg rounded-lg border-2 bg-white p-10">
-        <p>기업정보</p>
-        <p>{resume.title}</p>
-      </div>
-      <div className="mt-10 h-96 w-full max-w-lg rounded-lg border-2 bg-white p-10">
-        <p>자소서 내용 작성자 : {resume.user_name}</p>
-      </div>
+    <div className="min-h-screen w-full bg-my-bg p-8">
+      <div className="flex flex-col items-center space-y-20">
+        {/*기업 정보*/}
+        <div className="w-2/3 rounded-2xl bg-white p-10 shadow-xl">
+          <div className="mx-4 flex justify-between">
+            <div className="mb-10 flex flex-col">
+              <span className="text-2xl font-bold">기업</span>
+              <span className="text-xl">제목</span>
+            </div>
+            <div>
+              <JobInfo targetJob={targetJob} />
+              <button className="mt-5 w-fit rounded-full bg-my-main px-5 py-2">
+                채용 사이트
+              </button>
+            </div>
+          </div>
+        </div>
 
-      <div className="mt-6 flex space-x-4">
-        {role === 'seeker' ? (
-          <>
-            <button
-              onClick={handleEdit}
-              className="rounded bg-my-main px-4 py-2 text-white"
-            >
-              수정
-            </button>
-            <button
-              onClick={handleDelete}
-              className="rounded bg-my-main px-4 py-2 text-white"
-            >
-              삭제
-            </button>
-          </>
-        ) : (
-          <button
-            onClick={handleReview}
-            className="rounded bg-my-main px-4 py-2 text-white"
-          >
-            검토
-          </button>
-        )}
-      </div>
+        {/*자기 소개서*/}
+        <div className="w-2/3 rounded-2xl bg-white p-10 shadow-xl">
+          <div className="mx-4 flex justify-between">
+            <div>
+              <p className="font-bold">
+                <span className="text-my-main">{resume.user_name}</span>님의
+                자기소개서
+              </p>
+              <p>희망 근무 지역 : 서울 강남구</p>
+            </div>
+            <div className="h-[200px] w-[200px] border-2">지도</div>
+          </div>
 
-      <button
-        onClick={() => navigate(-1)}
-        className="mt-10 rounded bg-my-main px-4 py-2 text-white"
-      >
-        뒤로가기
-      </button>
+          {/* 자기소개서 내용 */}
+          <div className="mx-4 mt-8 flex flex-col items-center gap-4">
+            {[
+              '상담 과정',
+              '입사 후 포부',
+              '성과 및 장단점',
+              '경력사항 및 사회경험',
+            ].map((title, idx) => (
+              <div key={idx} className="w-full">
+                <span className="mb-2 block font-bold">{title}</span>
+                <div className="h-32 w-full rounded-2xl bg-my-gray p-3 shadow-sm">
+                  내용...
+                </div>
+              </div>
+            ))}
+            <div className="flex space-x-4">
+              {role === 'seeker' ? (
+                <>
+                  <button
+                    onClick={handleEdit}
+                    className="rounded-full bg-my-main px-4 py-2 text-white"
+                  >
+                    수정
+                  </button>
+                  <button
+                    onClick={handleDelete}
+                    className="rounded-full bg-my-main px-4 py-2 text-white"
+                  >
+                    삭제
+                  </button>
+                </>
+              ) : (
+                <button
+                  onClick={handleReview}
+                  className="rounded bg-my-main px-4 py-2 text-white"
+                >
+                  검토
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+        <button
+          onClick={() => navigate(-1)}
+          className="rounded-full bg-my-main px-4 py-2 text-white"
+        >
+          뒤로가기
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/ResumeListPage.jsx
+++ src/pages/ResumeListPage.jsx
@@ -31,35 +31,45 @@ const ResumeListPage = () => {
   };
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center bg-my-bg">
-      <button
-        onClick={toggleRole}
-        className="my-10 bg-blue-500 px-2 py-1 text-white"
-      >
-        모드 : {role}
-      </button>
+    <div className="min-h-screen w-full bg-my-bg p-8">
+      <div className="flex flex-col items-center">
+        <button
+          onClick={toggleRole}
+          className="mb-10 bg-blue-500 px-2 py-1 text-white"
+        >
+          모드 : {role}
+        </button>
 
-      <h1 className="mb-8 text-2xl font-bold">
-        {role === 'seeker' ? '나의 자소서 목록' : '전체 자소서 목록'}
-      </h1>
+        <h1 className="mb-8 text-2xl font-bold">
+          {role === 'seeker' ? '나의 자소서' : '전체 자소서'}
+        </h1>
 
-      {resumes ? (
-        <ul className="grid grid-cols-2 gap-4">
-          {resumes.map((resume) => (
-            <li
-              key={resume.id}
-              onClick={() => handleResumeClick(resume.id)}
-              className="cursor-pointer rounded-lg border-2 bg-white px-20 py-5"
-            >
-              <h3 className="text-xl font-semibold">{resume.title}</h3>
-              <p>{resume.user_name}</p>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div>자소서가 없습니다.</div>
-      )}
-      <button className="mt-20">더보기</button>
+        {resumes ? (
+          <ul className="grid grid-cols-2 gap-8">
+            {resumes.map((resume) => (
+              <li
+                key={resume.id}
+                onClick={() => handleResumeClick(resume.id)}
+                className="w-full cursor-pointer rounded-2xl bg-white px-28 py-8 shadow-xl"
+              >
+                <div className="flex flex-col">
+                  <span className="text-2xl font-bold">{resume.title}</span>
+                  <span className="text-lg">{resume.recruit_type}</span>
+                  <span>
+                    구직자 :
+                    <span className="font-bold"> {resume.user_name}</span>
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div>자소서가 없습니다.</div>
+        )}
+        <button className="my-20 rounded-full bg-my-main px-4 py-2">
+          더보기
+        </button>
+      </div>
     </div>
   );
 };
@@ -73,59 +83,69 @@ export const dummyData = [
     title: '기업이름 1',
     user_name: '칠칠이',
     user_id: 'user1',
+    recruit_type: '신입',
   },
   {
     id: 2,
     title: '기업이름 2',
     user_name: '유저2',
     user_id: 'user2',
+    recruit_type: '신입',
   },
   {
     id: 3,
     title: '기업이름 3',
     user_name: '칠칠이',
     user_id: 'user1',
+    recruit_type: '신입',
   },
   {
     id: 4,
     title: '기업이름 4',
     user_name: '칠칠이',
     user_id: 'user1',
+    recruit_type: '신입',
   },
   {
     id: 5,
     title: '기업이름 5',
     user_name: '유저4',
     user_id: 'user4',
+    recruit_type: '신입+경력',
   },
   {
     id: 6,
     title: '기업이름 6',
     user_name: '유저4',
     user_id: 'user4',
+    recruit_type: '신입',
   },
   {
     id: 7,
     title: '기업이름 7',
     user_name: '유저4',
     user_id: 'user4',
+    recruit_type: '신입+경력',
   },
   {
     id: 8,
     title: '기업이름 8',
     user_name: '유저5',
     user_id: 'user5',
+    recruit_type: '신입+경력',
   },
   {
     id: 9,
     title: '기업이름 9',
     user_name: '유저5',
     user_id: 'user5',
+    recruit_type: '신입',
   },
   {
     id: 10,
     title: '기업이름 10',
     user_name: '유저5',
     user_id: 'user5',
+    recruit_type: '신입',
   },
 ];
