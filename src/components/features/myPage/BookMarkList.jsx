cd5a29061462e53472f82d11b3744e3ef26d320b
Minjo Park <consistent.312@gmail.com>
2025-02-27 15:34:37 +0900

    add: 북마크 리스트 컴포넌트 추가

---
+++ src/components/features/myPage/BookMarkList.jsx
@@ -0,0 +1,9 @@
+const BookMarkList = () => {
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <h1 className="text-2xl">북마크</h1>
+    </div>
+  );
+};
+
+export default BookMarkList;
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,9 +1,11 @@
+import BookMarkList from '../components/features/myPage/BookMarkList';
 import Profile from '../components/features/myPage/profile';
 
 const MyPage = () => {
   return (
-    <div className="flex h-screen justify-center bg-my-bg">
+    <div className="flex h-screen flex-col gap-10 bg-my-bg">
       <Profile />
+      <BookMarkList />
     </div>
   );
 };
