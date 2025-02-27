058916d0b3f1bda762bba6c77966eb79fbbbf664
Minjo Park <consistent.312@gmail.com>
2025-02-27 17:10:46 +0900

    style: 북마크 리스트 UI 완료

--- src/components/common/JobItem.jsx
+++ src/components/common/JobItem.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react';
 import { IoBookmarkOutline, IoBookmark } from 'react-icons/io5';
+import { iconSize } from '../../constants/iconSizeConstants';
 
-const JobItem = ({ job }) => {
+// To.지은님
+// jobs를 map로 돌렸을 때 나오는 job과 카드 넓이를 설정하는 width를 props로 넘겨받습니다.
+// width는 원하시는 값으로 설정하시면 됩니다.
+// job 관련 정보들은 따로 수정할 필요없으실거예요~!
+
+const JobItem = ({ job, width }) => {
   const { company_name, start_date, end_date } = job;
-  const [isBookmarked] = useState(false);
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const handleBookMarkClick = () => {
+    setIsBookmarked((prev) => !prev);
+  };
 
   return (
-    <div className="flex w-full max-w-[600px] items-center justify-between gap-4 rounded-xl bg-white p-10">
+    <div
+      className={`flex w-full max-w-[${width}px] items-center justify-between gap-4 rounded-xl bg-white p-10`}
+    >
       <div className="flex flex-col gap-2">
         <h1 className="text-xl font-semibold">{company_name}</h1>
         <h2>채용후기</h2>
@@ -16,12 +28,17 @@ const JobItem = ({ job }) => {
         </div>
       </div>
       <div className="flex items-center gap-3">
-        {isBookmarked ? (
-          <IoBookmark size={30} />
-        ) : (
-          <IoBookmarkOutline size={30} />
-        )}
-        <div className="rounded-full bg-my-main p-5">지원 자소서 0건</div>
+        <button onClick={handleBookMarkClick}>
+          {isBookmarked ? (
+            <IoBookmark size={iconSize.BASE} className="text-my-main" />
+          ) : (
+            <IoBookmarkOutline size={iconSize.BASE} />
+          )}
+        </button>
+        <div className="flex items-center gap-2 rounded-xl bg-my-main p-5">
+          <span>지원 자소서</span>
+          <span className="text-lg font-semibold">0건</span>
+        </div>
       </div>
     </div>
   );
