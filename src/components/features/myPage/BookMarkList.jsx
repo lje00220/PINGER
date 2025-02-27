393d3b69390a6179c263aff49f42ccd3d0093703
Minjo Park <consistent.312@gmail.com>
2025-02-27 16:36:16 +0900

    feat: 북마크에서 채용 정보 카드 연결

--- src/components/features/myPage/BookMarkList.jsx
+++ src/components/features/myPage/BookMarkList.jsx
@@ -1,10 +1,23 @@
-import JobItem from '../../common/jobItem';
+import JobItem from '../../common/JobItem';
 
 const BookMarkList = () => {
+  // 예시 데이터
+  const jobs = [
+    {
+      id: 1,
+      company_name: '서울대학교병원',
+      start_date: '20250205',
+      end_date: '20250212',
+    },
+  ];
+
+  /** UI */
   return (
     <div className="flex flex-col items-center gap-4">
       <h1 className="text-2xl">북마크</h1>
-      <JobItem />
+      {jobs.map((job) => {
+        return <JobItem key={job.id} job={job} />;
+      })}
     </div>
   );
 };
