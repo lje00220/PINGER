a6d62e69bfed2b4ff13e398ca13acad964e4521b
Minjo Park <consistent.312@gmail.com>
2025-02-27 21:02:42 +0900

    chore: 주석 추가

--- src/utils/handleToggleBookMark.js
+++ src/utils/handleToggleBookMark.js
@@ -1,6 +1,10 @@
 import { createBookMark } from '../api/createData';
 import { deleteBookMark } from '../api/deleteData';
 
+/**
+ * 북마크 추가/취소 함수
+ * @param {*} { isBookMarked, setIsBookmarked, jobId, userId }
+ */
 export const handleToggleBookMark = async ({
   isBookMarked,
   setIsBookmarked,
