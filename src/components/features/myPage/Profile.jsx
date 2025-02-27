6f7474b7717dab86b15454974511a2833b0faf11
Minjo Park <consistent.312@gmail.com>
2025-02-27 15:13:58 +0900

    feat: 프로필 UI 완료

--- src/components/features/myPage/Profile.jsx
+++ src/components/features/myPage/Profile.jsx
@@ -6,28 +6,45 @@ const Profile = () => {
   return (
     <div className="flex flex-col items-center gap-4">
       <h1 className="text-2xl">칠칠이님의 프로필</h1>
-      <div className="flex w-full max-w-[600px] flex-col gap-4 rounded-xl bg-white p-20">
-        <div className="flex justify-between">
-          <span className="text-lg font-semibold">ID</span>
-          <div className="flex w-[300px] justify-between">
-            <span>chillchill2</span>
-            <span className="flex h-[30px] w-[80px] items-center justify-center rounded-full bg-my-main">
-              {isSeeker ? '구직자' : '담당자'}
-            </span>
+      <div className="flex w-full max-w-[600px] flex-col items-center gap-8 rounded-xl bg-white p-20">
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between">
+            <span className="text-lg font-semibold">ID</span>
+            <div className="flex w-[300px] justify-between">
+              <span>chillchill2</span>
+              <span className="flex h-[30px] w-[80px] items-center justify-center rounded-full bg-my-main">
+                {isSeeker ? '구직자' : '담당자'}
+              </span>
+            </div>
           </div>
+          <label
+            htmlFor="nickname"
+            className="flex items-center justify-between gap-4"
+          >
+            <span className="text-lg font-semibold">닉네임</span>
+            <input
+              type="text"
+              id="nickname"
+              placeholder="닉네임"
+              className="h-[50px] w-[300px] rounded-full border-2 border-my-main px-5"
+            />
+          </label>
+          <label
+            htmlFor="address"
+            className="flex items-center justify-between gap-4"
+          >
+            <span className="text-lg font-semibold">주소</span>
+            <input
+              type="text"
+              id="address"
+              placeholder="주소"
+              className="h-[50px] w-[300px] rounded-full border-2 border-my-main px-5"
+            />
+          </label>
         </div>
-        <label
-          htmlFor="nickname"
-          className="flex items-center justify-between gap-4"
-        >
-          <span className="text-lg font-semibold">닉네임</span>
-          <input
-            type="text"
-            id="nickname"
-            placeholder="닉네임"
-            className="h-[50px] w-[300px] rounded-full border-2 border-my-main px-5"
-          />
-        </label>
+        <button className="h-[50px] w-[300px] rounded-full bg-my-main">
+          수정하기
+        </button>
       </div>
     </div>
   );
