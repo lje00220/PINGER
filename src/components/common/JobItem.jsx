50eaf7380835adfd476f88f9b1a0c9fe27c0ca3e
Minjo Park <consistent.312@gmail.com>
2025-02-27 19:29:40 +0900

    refactor: 상수 추가

--- src/components/common/JobItem.jsx
+++ src/components/common/JobItem.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { IoBookmarkOutline, IoBookmark } from 'react-icons/io5';
+import { IoBookmark, IoBookmarkOutline } from 'react-icons/io5';
 import { iconSize } from '../../constants/iconSizeConstants';
+import { handleToggleBookMark } from '../../utils/handleToggleBookMark';
 
 // To.지은님
 // jobs를 map로 돌렸을 때 나오는 job과 카드 넓이를 설정하는 width를 props로 넘겨받습니다.
@@ -8,28 +9,35 @@ import { iconSize } from '../../constants/iconSizeConstants';
 // job 관련 정보들은 따로 수정할 필요없으실거예요~!
 
 const JobItem = ({ job, width }) => {
-  const { company_name, start_date, end_date } = job;
-  const [isBookmarked, setIsBookmarked] = useState(false);
+  /** 추가해야되는 정보 */
+  // 유저 정보 -> user_id 가져오기
 
-  const handleBookMarkClick = () => {
-    setIsBookmarked((prev) => !prev);
-  };
+  const { id: postId, company_name, recruit_title, start_date, end_date } = job;
+  const [isBookMarked, setIsBookmarked] = useState(false);
+
+  const START_NUM = 0;
+  const END_NUM = 25;
+  const DOT = '...';
 
   return (
     <div
-      className={`flex w-full max-w-[${width}px] items-center justify-between gap-4 rounded-xl bg-white p-10`}
+      className={`flex max-w-[${width}px] mx-auto items-center justify-between gap-4 rounded-xl bg-white p-10`}
     >
       <div className="flex flex-col gap-2">
         <h1 className="text-xl font-semibold">{company_name}</h1>
-        <h2>채용후기</h2>
+        <h2>{recruit_title.slice(START_NUM, END_NUM) + DOT}</h2>
         <div className="flex gap-4">
           <span className="text-sm font-semibold">채용 날짜</span>
           <span className="text-sm">{`${start_date} - ${end_date}`}</span>
         </div>
       </div>
       <div className="flex items-center gap-3">
-        <button onClick={handleBookMarkClick}>
-          {isBookmarked ? (
+        <button
+          onClick={() =>
+            handleToggleBookMark(isBookMarked, setIsBookmarked, postId)
+          }
+        >
+          {isBookMarked ? (
             <IoBookmark size={iconSize.BASE} className="text-my-main" />
           ) : (
             <IoBookmarkOutline size={iconSize.BASE} />
