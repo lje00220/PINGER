d36bd5999a28a3f722bee62c5ffcfb8f6d34fa06
Minjo Park <consistent.312@gmail.com>
2025-02-27 21:18:32 +0900

    fix: 북마크 버튼 눌렀을 때 detail 이동 막기

--- src/components/common/JobItem.jsx
+++ src/components/common/JobItem.jsx
@@ -4,6 +4,8 @@ import { iconSize } from '../../constants/iconSizeConstants';
 import { handleToggleBookMark } from '../../utils/handleToggleBookMark';
 import separateDate from '../../utils/SeparateDate';
 import sliceTitleLength from '../../utils/SliceTitleLength';
+import { Link } from 'react-router-dom';
+import { PATH } from '../../constants/RouterPathConstants';
 
 // To.지은님
 // jobs를 map로 돌렸을 때 나오는 job과 카드 넓이를 설정하는 width를 props로 넘겨받습니다.
@@ -20,37 +22,40 @@ const JobItem = ({ job, width }) => {
   const [resumeCount, setResumeCount] = useState(0);
 
   return (
-    <div
-      className={`flex w-full max-w-[${width}px] mx-auto items-center justify-between gap-4 rounded-xl bg-white p-10`}
-    >
-      <div className="flex flex-col gap-2">
-        <h1 className="text-xl font-semibold">{company_name}</h1>
-        <h2>{sliceTitleLength(recruit_title)}</h2>
-        <div className="flex gap-4">
-          <span className="text-sm font-semibold">채용 날짜</span>
-          <span className="text-sm">
-            {`${separateDate(start_date)} ~ ${separateDate(end_date)}`}
-          </span>
+    <Link to={`${PATH.JOB_DETAIL}/${postId}`}>
+      <div
+        className={`flex w-full max-w-[${width}px] mx-auto items-center justify-between gap-4 rounded-xl bg-white p-10`}
+      >
+        <div className="flex flex-col gap-2">
+          <h1 className="text-xl font-semibold">{company_name}</h1>
+          <h2>{sliceTitleLength(recruit_title)}</h2>
+          <div className="flex gap-4">
+            <span className="text-sm font-semibold">채용 날짜</span>
+            <span className="text-sm">
+              {`${separateDate(start_date)} ~ ${separateDate(end_date)}`}
+            </span>
+          </div>
+        </div>
+        <div className="flex items-center gap-3">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              handleToggleBookMark(isBookMarked, setIsBookmarked, postId);
+            }}
+          >
+            {isBookMarked ? (
+              <IoBookmark size={iconSize.BASE} className="text-my-main" />
+            ) : (
+              <IoBookmarkOutline size={iconSize.BASE} />
+            )}
+          </button>
+          <div className="flex items-center gap-2 rounded-xl bg-my-main p-5">
+            <span>지원 자소서</span>
+            <span className="text-lg font-semibold">{`${resumeCount}건`}</span>
+          </div>
         </div>
       </div>
-      <div className="flex items-center gap-3">
-        <button
-          onClick={() =>
-            handleToggleBookMark(isBookMarked, setIsBookmarked, postId)
-          }
-        >
-          {isBookMarked ? (
-            <IoBookmark size={iconSize.BASE} className="text-my-main" />
-          ) : (
-            <IoBookmarkOutline size={iconSize.BASE} />
-          )}
-        </button>
-        <div className="flex items-center gap-2 rounded-xl bg-my-main p-5">
-          <span>지원 자소서</span>
-          <span className="text-lg font-semibold">{`${resumeCount}건`}</span>
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 };
 
