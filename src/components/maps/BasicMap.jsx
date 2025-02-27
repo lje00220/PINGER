ab2c42833ad13dfd636660283077ac83e3221ad7
Hyojin-Moon <moontough8@gmail.com>
2025-02-27 12:37:15 +0900

    feat: 맵 마커, 클릭시 오버레이 설정

--- src/components/maps/BasicMap.jsx
+++ src/components/maps/BasicMap.jsx
@@ -1,13 +1,16 @@
-import { Map, MapMarker} from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 import useKakaoLoader from "../../hooks/useKakaoLoader";
+import { useState } from "react";
 
 const BasicMap = () => {
   useKakaoLoader();
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Map
       center={{ lat: 37.5665, lng: 126.9780 }}
-      style={{ width: "1920px", height: "1080px" }}
+      className="w-screen h-screen"
       level={3}
     >
       <MapMarker
@@ -15,8 +18,22 @@ const BasicMap = () => {
           lat: 37.5665,
           lng: 126.9780,
         }}
-      />
-      </Map>
+        clickable={true}
+        onClick={() => setIsOpen(true)}
+      >
+        {isOpen && (
+          <div className="relative min-w-[150px]">
+            <img
+              alt="close"
+              src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
+              className="absolute top-1 right-1 w-4 h-4 cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            />
+            <div className="text-black p-1">Chill Chill</div>
+          </div>
+        )}
+      </MapMarker>
+    </Map>
   );
 };
 
