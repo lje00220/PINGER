909afcadd57388d30d695eac831518efbcfc0fe6
Minjo Park <consistent.312@gmail.com>
2025-02-28 20:46:32 +0900

    refactor: 경로 수정

--- src/components/auth/AuthForm.jsx
+++ src/components/auth/AuthForm.jsx
@@ -3,9 +3,10 @@ import { AUTH_MODE, BUTTON_MODE } from '../../constants/mode';
 import { Button } from '../common/Button';
 import { InputBar, InputRadio } from '../common/Input';
 import { Link } from 'react-router-dom';
-import { PATH } from '../../constants/RouterPathConstants';
+
 import ErrorText from '../common/ErrorText';
 import SignupAddressSelect from './SignupAddressInput';
+import { PATH } from '../../constants/routerPath';
 
 const AuthForm = ({
   mode,
