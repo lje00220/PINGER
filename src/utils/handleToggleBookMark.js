c73bdaa9f2a171d7479827477261ea4edcefe7c0
Minjo Park <consistent.312@gmail.com>
2025-02-28 15:49:33 +0900

    chore: 주석 수정 및 필요없는 코드 삭제

--- src/utils/handleToggleBookMark.js
+++ src/utils/handleToggleBookMark.js
@@ -3,7 +3,10 @@ import { deleteBookMark } from '../api/deleteData';
 
 /**
  * 북마크 추가/취소 함수
- * @param {*} { isBookMarked, setIsBookmarked, jobId, userId }
+ * @param {boolean} isBookMarked
+ * @param {function} setIsBookmarked
+ * @param {number} jobId
+ * @param {string} userId
  */
 export const handleToggleBookMark = async ({
   isBookMarked,
@@ -11,17 +14,15 @@ export const handleToggleBookMark = async ({
   jobId,
   userId,
 }) => {
-  const newBookMark = { job_id: jobId, user_id: userId };
-
   try {
     /** 북마크 추가/취소 */
     if (isBookMarked === false) {
-      await createBookMark(newBookMark);
+      await createBookMark(jobId, userId);
     } else {
       await deleteBookMark(jobId, userId);
     }
 
-    setIsBookmarked((prev) => !prev);
+    setIsBookmarked(!isBookMarked);
   } catch (error) {
     console.error('북마크 오류', error);
   }
