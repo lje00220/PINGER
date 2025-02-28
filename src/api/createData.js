ca6d6b20e609ac3e1dbc6137a4104b8a0ddc8da8
Minjo Park <consistent.312@gmail.com>
2025-02-28 15:27:38 +0900

    fix: 북마크 추가 함수 로직 수정

--- src/api/createData.js
+++ src/api/createData.js
@@ -4,6 +4,6 @@ import supabase from '../supabase/client';
  * 북마크 추가 함수
  * @param {*} newBookMark
  */
-export const createBookMark = async (newBookMark) => {
-  await supabase.from('bookmarks').insert(newBookMark).select('*');
+export const createBookMark = async (jobId, userId) => {
+  await supabase.from('bookmarks').insert([{ job_id: jobId, user_id: userId }]);
 };
