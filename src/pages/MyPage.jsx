20557cd4e1921e447afd282290c8a74ec10c5a57
Minjo Park <consistent.312@gmail.com>
2025-02-28 16:05:21 +0900

    feat: role에 따른 조건부 렌더링 추가

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import BookMarkList from '../components/features/myPage/BookMarkList';
 import Profile from '../components/features/myPage/profile';
+import ResumeList from '../components/features/myPage/ResumeList';
 
 const MyPage = () => {
+  const [isSeeker, setIsSeeker] = useState(true);
+
   return (
     <div className="flex h-full w-full flex-col gap-10 bg-my-bg py-8">
-      <Profile />
-      <BookMarkList />
+      <Profile isSeeker={isSeeker} setIsSeeker={setIsSeeker} />
+      {isSeeker ? <BookMarkList /> : <ResumeList />}
     </div>
   );
 };
