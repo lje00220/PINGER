d7f3a857c1ca0b51b6005e09d23c687b9ef2732f
Minjo Park <consistent.312@gmail.com>
2025-02-28 16:04:43 +0900

    fix: isSeeker state 제거

--- src/components/features/myPage/Profile.jsx
+++ src/components/features/myPage/Profile.jsx
@@ -2,11 +2,10 @@ import { useEffect, useState } from 'react';
 import supabase from '../../../supabase/client';
 import { LongButton } from '../../common/Button';
 
-const Profile = () => {
-  /** 더미 데이터 생성하면 수정해야되는 코드  */
-  // 조건부 렌더링을 위해 잠깐 추가해두었습니다.
-  const [isSeeker, setIsSeeker] = useState(true);
+const Profile = ({ isSeeker, setIsSeeker }) => {
+  /** 전역으로 user 정보 생기면 수정해야 되는 부분  */
   const [userInfo, setUserInfo] = useState([]);
+  const { nickname, email, role, address } = userInfo;
 
   const userId = '544a3df2-13c9-4cb0-a396-f5ad773cce68';
 
@@ -20,7 +19,7 @@ const Profile = () => {
           .single();
 
         setUserInfo(users);
-        if (users.role !== 'seeker') setIsSeeker(false);
+        if (role !== 'seeker') setIsSeeker(false);
       } catch (error) {
         console.error(error);
       }
@@ -29,17 +28,22 @@ const Profile = () => {
     getUsers(userId);
   }, []);
 
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setUserInfo((prev) => ({ ...prev, [id]: value }));
+  };
+
   /** 공통 컴포넌트 제작 + 더미 데이터 생성하면 수정해야되는 코드 */
   return (
     <div className="flex w-full flex-col items-center gap-4">
-      <h1 className="text-2xl">{`${userInfo.nickname}님의 프로필`}</h1>
+      <h1 className="text-2xl">{`${nickname}님의 프로필`}</h1>
 
       <div className="flex w-full max-w-[600px] flex-col items-center gap-8 rounded-xl bg-white p-20">
         <div className="flex flex-col gap-4">
           <div className="flex justify-between">
             <span className="text-lg font-semibold">ID</span>
             <div className="flex w-[300px] justify-between">
-              <span>{userInfo.email}</span>
+              <span>{email}</span>
               <span className="flex h-[30px] w-[80px] items-center justify-center rounded-full bg-my-main">
                 {isSeeker ? '구직자' : '담당자'}
               </span>
@@ -55,7 +59,8 @@ const Profile = () => {
               type="text"
               id="nickname"
               placeholder="닉네임"
-              value={userInfo.nickname}
+              value={nickname}
+              onChange={handleChange}
               className="h-[50px] w-[300px] rounded-full border-2 border-my-main px-5"
             />
           </label>
@@ -69,7 +74,8 @@ const Profile = () => {
               type="text"
               id="address"
               placeholder="주소"
-              value={userInfo.address}
+              value={address}
+              onChange={handleChange}
               className="h-[50px] w-[300px] rounded-full border-2 border-my-main px-5"
             />
           </label>
