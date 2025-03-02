31e6764fde956abdc26d43ec9d6f9a5cbe4ac34b
Minjo Park <consistent.312@gmail.com>
2025-03-03 01:27:16 +0900

    chore: USERS 상수 적용

---
+++ src/api/users.js
@@ -0,0 +1,14 @@
+import { QUERY_KEY } from '../constants/queryKeys';
+import supabase from '../supabase/client';
+
+export const updateUserInfo = async ({ userId, newNickname, newAddress }) => {
+  try {
+    await supabase
+      .from(QUERY_KEY.USERS)
+      .update({ nickname: newNickname, address: newAddress })
+      .eq('user_id', userId)
+      .single();
+  } catch (error) {
+    console.error('사용자 프로필 업데이트 오류', error);
+  }
+};
