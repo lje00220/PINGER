72f35b54b0332bf2a46ae1aebc957b5dba8619fb
Minjo Park <consistent.312@gmail.com>
2025-03-03 00:07:39 +0900

    fix: LoadingPage 적용

--- src/components/features/myPage/BookMarkList.jsx
+++ src/components/features/myPage/BookMarkList.jsx
@@ -1,24 +1,24 @@
 import { useBookMarksQuery } from '../../../hooks/useBookMarksQuery';
 import JobItem from '../../common/JobItem';
+import LoadingPage from '../../common/LoadingPage';
 
 const BookMarkList = () => {
   const { data: bookMarkList, isPending, isError } = useBookMarksQuery();
 
   /** UI */
-  if (isPending) {
-    return <span>북마크한 채용 정보가 없습니다.</span>;
-  }
-
-  if (isError) {
-    return <span>북마크 리스트를 가져오는 중 에러가 발생하였습니다.</span>;
-  }
+  if (isPending) return <LoadingPage state="load" />;
+  if (isError) return <LoadingPage state="error" />;
 
   return (
     <div className="flex w-full flex-col items-center gap-5">
       <h1 className="text-2xl">북마크</h1>
-      {bookMarkList.map((data) => {
-        return <JobItem key={data.id} job={data.jobs} />;
-      })}
+      {bookMarkList.length === 0 ? (
+        <span>북마크한 채용 정보가 없습니다</span>
+      ) : (
+        bookMarkList.map((data) => {
+          return <JobItem key={data.id} job={data.jobs} />;
+        })
+      )}
     </div>
   );
 };
