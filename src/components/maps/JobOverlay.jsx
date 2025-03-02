64aaf36a32f8a61848884a88d1889004bd39295c
Hyojin-Moon <moontough8@gmail.com>
2025-03-02 17:17:10 +0900

    docs: (#8) close 아이콘 수정

--- src/components/maps/JobOverlay.jsx
+++ src/components/maps/JobOverlay.jsx
@@ -1,17 +1,10 @@
 import { Link } from "react-router-dom";
-
-
-const CLOSE_ICON_URL = 'https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif';
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 // 클릭시 오버레이 디자인
 const JobOverlay = ({ job, onClose }) => (
   <div className="relative min-w-[180px] min-h-[60px] rounded-lg border bg-white shadow-md p-4 transform -translate-y-8 flex flex-col items-center space-y-2 text-center">
-    <img
-      alt="close"
-      src={CLOSE_ICON_URL}
-      className="absolute top-1 right-1 w-4 h-4 cursor-pointer"
-      onClick={onClose}
-    />
+    <IoMdCloseCircleOutline className="absolute top-1 right-1 w-4 h-4 cursor-pointer" onClick={onClose}/>
     <div className="font-semibold text-lg">{job.company_name}</div>
     <div className="text-sm">{job.adress}</div>
     <div className="text-xs">{job.work_type}</div>
