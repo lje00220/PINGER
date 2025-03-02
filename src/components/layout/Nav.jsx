3a75a0dd96f161156a72ddf990fdd9333ca41113
miing_gaeng <pem4134@naver.com>
2025-03-02 20:32:45 +0900

    feat: nav에 유저 정보 표시 #40

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -5,8 +5,9 @@ import { PATH } from '../../constants/routerPath';
 
 const Nav = () => {
   //-----zustand-----
-  const { isAuthenticated, setLogout } = useAuthStore((state) => state);
+  const { isAuthenticated, setLogout, user } = useAuthStore((state) => state);
 
+  //로그아웃 로직
   const handleLogout = async () => {
     await supabase.auth.signOut();
     setLogout();
@@ -15,7 +16,7 @@ const Nav = () => {
   // 로그인 여부에 따른 링크 생성 리스트 분리
   const menuItems = isAuthenticated
     ? [
-        { to: PATH.MY_PAGE, label: '칠칠이님' },
+        { to: PATH.MY_PAGE, label: `${user?.nickname} 님` },
         { to: PATH.JOB_LIST, label: '채용 리스트' },
         { to: PATH.RESUME_LIST, label: '자소서 리뷰' },
         {
@@ -33,16 +34,22 @@ const Nav = () => {
       ];
 
   return (
-    <nav className="mr-3 flex flex-row gap-3">
+    <nav className="mr-3 flex flex-row items-center gap-3">
+      {!!user &&
+        (user?.role === 'seeker' ? (
+          <div className={ROLE}>구직자</div>
+        ) : (
+          <div className={ROLE}>멘토</div>
+        ))}
       {menuItems.map((item, idx) => {
         return item.isButton ? (
           <Link to={item.to} key={idx}>
-            <button onClick={item.onClick} className="">
+            <button onClick={item.onClick} className={NAV_BUTTON}>
               {item.label}
             </button>
           </Link>
         ) : (
-          <Link to={item.to} key={idx} className="">
+          <Link to={item.to} key={idx} className={NAV_BUTTON}>
             {item.label}
           </Link>
         );
@@ -52,3 +59,7 @@ const Nav = () => {
 };
 
 export default Nav;
+
+//tailwind css
+const ROLE = 'w-12 rounded-md bg-my-main text-center text-xs';
+const NAV_BUTTON = 'cursor-pointer hover:text-my-main';
