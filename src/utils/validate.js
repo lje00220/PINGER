0ece4b5d6be82a7845cc7b1a30da47c8652a8232
miing_gaeng <pem4134@naver.com>
2025-03-02 22:15:49 +0900

    feat: 이메일 에러처리 빈칸 추가

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,7 +3,7 @@ import { AUTH_ERROR_MESSAGES } from '../constants/toastMessages';
 export const validateSignUpForm = (name, value, password) => {
   switch (name) {
     case 'email':
-      if (value.length < 8 || !value.includes('@')) {
+      if (value.length < 8 || !value.includes('@') || value.includes(' ')) {
         return AUTH_ERROR_MESSAGES.EMAIL.INVALIDATE;
       }
       break;
