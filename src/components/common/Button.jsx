ded9cc2fc3d6015d192852143de651a35d1329f2
miing_gaeng <pem4134@naver.com>
2025-03-02 22:41:09 +0900

    chore: index.css에 hover 적용 => 실패 #43

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,10 +1,6 @@
 export const Button = ({ mode, type, onClick, children }) => {
   return (
-    <button
-      type={type}
-      onClick={onClick}
-      className={`${mode} hover:bg-my-hover`}
-    >
+    <button type={type} onClick={onClick} className={mode}>
       {children}
     </button>
   );
