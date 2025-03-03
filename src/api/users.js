1ce39627b09cd33b7e650f95aed6504d98abd865
Minjo Park <consistent.312@gmail.com>
2025-03-03 14:07:26 +0900

    feat: metadata 업데이트하는 로직 구현

--- src/api/users.js
+++ src/api/users.js
@@ -1,7 +1,7 @@
 import { QUERY_KEY } from '../constants/queryKeys';
 import supabase from '../supabase/client';
 
-export const updateUserInfo = async ({ userId, newNickname, newAddress }) => {
+export const updateUserData = async ({ userId, newNickname, newAddress }) => {
   try {
     await supabase
       .from(QUERY_KEY.USERS)
@@ -9,5 +9,18 @@ export const updateUserInfo = async ({ userId, newNickname, newAddress }) => {
       .eq('user_id', userId);
   } catch (error) {
     console.error('사용자 프로필 업데이트 오류', error);
+  }
+};
+
+export const updateUserMetaData = async ({ newNickname, newAddress }) => {
+  try {
+    await supabase.auth.updateUser({
+      data: {
+        nickname: newNickname,
+        address: newAddress,
+      },
+    });
+  } catch (error) {
+    console.error('사용자 MetaData 업데이트 오류', error);
   }
 };
