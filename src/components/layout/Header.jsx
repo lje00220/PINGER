e6d68940399f62b30500e085480118662c808cbf
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-03 21:54:07 +0900

    style: 헤더에 sticky 속성 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,7 +4,7 @@ import Nav from './Nav';
 
 const Header = () => {
   return (
-    <header className="flex h-20 w-full flex-row flex-wrap items-center justify-between bg-white p-4">
+    <header className="sticky top-0 z-50 flex h-20 w-full flex-row flex-wrap items-center justify-between bg-white p-4">
       <Link to={PATH.HOME} className="ml-3">
         <img src="/images/PINGER_logo.png" alt="logo" className="w-[150px]" />
       </Link>
