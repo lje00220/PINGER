2b15e66aeae2180fb619158427db71d6536f3c2c
Minjo Park <consistent.312@gmail.com>
2025-03-03 21:58:55 +0900

    feat: 멘토가 검토한 자소서 조회하는 로직 추가

--- src/api/resumes.js
+++ src/api/resumes.js
@@ -41,6 +41,27 @@ export const fetchResume = async (id) => {
 };
 
 /**
+ * Supabase의 resumes 테이블에서 멘토가 검토한 자소서 조회
+ * @param {string} mentorId - 로그인한 멘토 아이디
+ * @returns
+ */
+export const fetchConfirmedResumes = async (mentorId) => {
+  try {
+    const { data: confirmedResumes } = await supabase
+      .from('resumes')
+      .select(
+        '*, jobs(recruit_title, recruit_type, company_name),users: writer_id(nickname)',
+      )
+      .eq('mentor_id', mentorId)
+      .eq('is_confirmed', true);
+
+    return confirmedResumes || [];
+  } catch (error) {
+    console.error('검토한 자소서 조회 중 오류 발생', error);
+  }
+};
+
+/**
  * Supabase의 resumes 테이블에서 자소서 업데이트
  * @param {number} id - 업데이트 할 자소서의 id
  * @param {Object} updatedData - 업데이트할 내용
