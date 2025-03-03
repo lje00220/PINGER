5652940886cb710282939ff69e07f49fe4c3e404
Minjo Park <consistent.312@gmail.com>
2025-03-03 14:06:35 +0900

    chore: 함수명 수정

--- src/hooks/users/useUpdateUserMutation.js
+++ src/hooks/users/useUpdateUserMutation.js
@@ -1,12 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { updateUserInfo } from '../../api/users';
+import { updateUserData } from '../../api/users';
 import { QUERY_KEY } from '../../constants/queryKeys';
 
 export const useUpdateUserMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: updateUserInfo,
+    mutationFn: updateUserData,
     onSuccess: queryClient.invalidateQueries([QUERY_KEY.USERS]),
   });
 };
