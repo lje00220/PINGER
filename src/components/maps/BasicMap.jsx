0c472ac092e0e7f3a42c67f877c4aebef9ad9efd
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-03-04 01:12:17 +0900

    feat: 인트로 모달 생성

--- src/components/maps/BasicMap.jsx
+++ src/components/maps/BasicMap.jsx
@@ -1,4 +1,10 @@
-import { CustomOverlayMap, Map, MapMarker, MapTypeControl, ZoomControl } from 'react-kakao-maps-sdk';
+import {
+  CustomOverlayMap,
+  Map,
+  MapMarker,
+  MapTypeControl,
+  ZoomControl,
+} from 'react-kakao-maps-sdk';
 import useKakaoLoader from '../../hooks/useKakaoLoader';
 import { useJobsQuery } from '../../hooks/useJobsQuerys';
 import { useMapStore } from '../../zustand/useMapStore';
@@ -6,14 +12,22 @@ import { useEffect } from 'react';
 import JobOverlay from './JobOverlay';
 import { InputBar } from '../common/Input';
 import { AUTH_INPUT_PLACEHOLDER } from '../../constants/inputPlaceholder';
+import IntroModal from '../common/IntroModal';
 
 const BasicMap = () => {
   useKakaoLoader();
 
   const { data: jobData, isPending, isError } = useJobsQuery();
   const {
-    isOpen, keyword, filteredJobs, selectedCompany,
-    setMap, setKeyword, setIsOpen, setSelectedCompany, setJobData
+    isOpen,
+    keyword,
+    filteredJobs,
+    selectedCompany,
+    setMap,
+    setKeyword,
+    setIsOpen,
+    setSelectedCompany,
+    setJobData,
   } = useMapStore();
 
   // 데이터 설정
@@ -23,7 +37,6 @@ const BasicMap = () => {
     }
   }, [jobData, setJobData]);
 
-
   // **초기화 로직 (다른 페이지에 갔다 오면 초기화)**
   useEffect(() => {
     setKeyword('');
@@ -32,32 +45,32 @@ const BasicMap = () => {
   }, []);
 
   if (isPending) return <div className="p-4 text-center">로딩 중...</div>;
-  if (isError) return <div className="p-4 text-center">데이터 불러오기 실패</div>;
+  if (isError)
+    return <div className="p-4 text-center">데이터 불러오기 실패</div>;
 
   return (
-    <div className="relative w-screen h-[calc(100vh-80px)] overflow-hidden">
-
+    <div className="relative h-[calc(100vh-80px)] w-screen overflow-hidden">
+      <IntroModal />
       {/* 왼쪽 검색 & 결과 패널 */}
-      <div className="absolute top-8 left-14 z-50 w-[300px] h-[80vh] bg-gray-100/80 shadow-lg rounded-xl p-4 overflow-auto">
-
+      <div className="absolute left-14 top-8 z-50 h-[80vh] w-[300px] overflow-auto rounded-xl bg-gray-100/80 p-4 shadow-lg">
         {/* 검색 입력창 */}
         <div className="mb-4">
           <InputBar
-            type='text'
+            type="text"
             value={keyword}
             onChange={(e) => setKeyword(e.target.value)}
             placeholder={AUTH_INPUT_PLACEHOLDER.SEARCH}
-            className="border p-2 w-full"
+            className="w-full border p-2"
           />
         </div>
 
         {/* 검색 결과 목록 */}
         {filteredJobs.length > 0 && (
-          <ul className="bg-white shadow-md rounded-lg max-h-[70vh] overflow-auto">
+          <ul className="max-h-[70vh] overflow-auto rounded-lg bg-white shadow-md">
             {filteredJobs.map((job) => (
               <li
                 key={job.id}
-                className="p-2 border-b cursor-pointer hover:bg-my-main"
+                className="cursor-pointer border-b p-2 hover:bg-my-main"
                 onClick={() => setSelectedCompany(job)}
               >
                 <strong>{job.company_name}</strong>
@@ -68,14 +81,13 @@ const BasicMap = () => {
       </div>
 
       {/* 지도 */}
-      <div className="w-screen h-screen">
+      <div className="h-screen w-screen">
         <Map
           center={{ lat: 37.5665, lng: 126.978 }}
           className="h-full w-full"
           level={2}
           onCreate={setMap}
         >
-
           {/* 채용 정보 마커 */}
           {jobData.map((job) => (
             <div key={job.id}>
@@ -123,16 +135,19 @@ const BasicMap = () => {
                   position={{ lat: job.lat, lng: job.lng }}
                   clickable={true}
                 >
-                  <JobOverlay job={job} onClose={() => {
-                    setIsOpen(null);
-                    setSelectedCompany(null);
-                  }} />
+                  <JobOverlay
+                    job={job}
+                    onClose={() => {
+                      setIsOpen(null);
+                      setSelectedCompany(null);
+                    }}
+                  />
                 </CustomOverlayMap>
               )}
             </div>
           ))}
-          <MapTypeControl position={"TOPRIGHT"} />
-          <ZoomControl position={"RIGHT"} />
+          <MapTypeControl position={'TOPRIGHT'} />
+          <ZoomControl position={'RIGHT'} />
         </Map>
       </div>
     </div>
