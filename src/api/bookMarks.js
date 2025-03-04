a15698b774f374f14f3dffb162793b0be1c90380
Minjo Park <consistent.312@gmail.com>
2025-03-04 13:05:15 +0900

    chore:  불필요한 코드 삭제

--- src/api/bookMarks.js
+++ src/api/bookMarks.js
@@ -2,26 +2,6 @@ import { QUERY_KEY } from '../constants/queryKeys';
 import supabase from '../supabase/client';
 
 /**
- * 해당 사용자가 저장한 북마크 데이터 조회
- * @param {string} userId - 로그인한 사용자 아이디
- * @returns bookMarkData
- */
-export const fetchUserBookMarks = async (userId) => {
-  try {
-    const { data: bookMarkList } = await supabase
-      .from(QUERY_KEY.BOOKMARKS)
-      .select(
-        `*, ${QUERY_KEY.JOBS}:job_id(id, company_name, recruit_title, start_date, end_date), resumes:job_id(*)`,
-      )
-      .eq('user_id', userId);
-
-    return bookMarkList || [];
-  } catch (error) {
-    console.error('북마크 조회 오류', error);
-  }
-};
-
-/**
  * 북마크 추가 함수
  * @param {*} newBookMark
  */
